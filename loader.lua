065998ebc28d6501ebbc0bf3dcd328549f2f48da
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2022-10-02 19:37:04 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,10 @@
-if game.PlaceId == 4588604953 then 
-    	elseif GameTitle == "Criminality" then
+if game.PlaceId == 8343259840 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/PhillipThePaster/JarquisRococks/main/Criminality", true))()
 end 
 
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PhillipThePaster/JarquisRococks/main/jarquis.lua ", true))() 
+
+
+
