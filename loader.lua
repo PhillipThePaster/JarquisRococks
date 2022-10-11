4414c2ac8a17ae835ce0de270ff8302c6c17462a
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2022-10-11 02:45:08 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 8343259840 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/PhillipThePaster/JarquisRococks/main/Criminality", true))()
 end 
 
+elseif GameTitle == "BIG Paintball" then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/PhillipThePaster/JarquisRococks/main/Paintball.lua", true))()
 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PhillipThePaster/JarquisRococks/main/jarquis.lua ", true))() 
